3473dd3a7b6fb88d625da07d4d9f11e79b01849d
Thiago Luiz <tl7thiago@gmail.com>
2024-07-19 18:20:33 -0300

    Módulo Finalizado

---
+++ aulas/02-ignite-timer/src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Router";
+
+import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
