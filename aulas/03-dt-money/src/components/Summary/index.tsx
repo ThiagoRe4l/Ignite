f6fe241a6a42ab12ae9a1753c7562f774e51c009
Thiago Luiz <tl7thiago@gmail.com>
2024-07-23 21:19:32 -0300

    Módulo Concluído

---
+++ aulas/03-dt-money/src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
+import { primeFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
+
+export function Summary() {
+    const summary = useSummary();
+
+    return (
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Entradas</span>
+                    <ArrowCircleUp size={32} color="#00b37e" />
+                </header>
+
+                <strong>{primeFormatter.format(summary.income)}</strong>
+            </SummaryCard>
+            <SummaryCard>
+                <header>
+                    <span>Saídas</span>
+                    <ArrowCircleDown size={32} color="#f75a68" />
+                </header>
+
+                <strong>{primeFormatter.format(summary.outcome)}</strong>
+            </SummaryCard>
+            <SummaryCard variant="green">
+                <header>
+                    <span>Entradas</span>
+                    <CurrencyDollar size={32} color="#fff" />
+                </header>
+
+                <strong>{primeFormatter.format(summary.total)}</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+}
