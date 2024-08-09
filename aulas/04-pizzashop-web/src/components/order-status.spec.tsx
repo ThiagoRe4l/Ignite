3807a7a5488db3b2a7372729a4299c9e1404fe44
Thiago Luiz <tl7thiago@gmail.com>
2024-08-09 17:44:02 -0300

    .

---
+++ aulas/04-pizzashop-web/src/components/order-status.spec.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable prettier/prettier */
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from './order-status'
+
+describe('Order Status', () => {
+    it('should display the right text when order status is pending', () => {
+        const wrapper = render(<OrderStatus status="pending"/>) // renderizando meu OrderStatus
+
+        wrapper.debug()
+
+        const statusText = wrapper.getByText('Pendente') // verificando se existe um elemento onde o texto está no documento
+        const badgeElement = wrapper.getByTestId('badge')
+
+        console.log(badgeElement.outerHTML)
+
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-slate-400')   
+    })
+
+    it('should display the right text when order status is canceled', () => {
+        const wrapper = render(<OrderStatus status="canceled"/>)
+
+        wrapper.debug()
+
+        const statusText = wrapper.getByText('Cancelado')
+        const badgeElement = wrapper.getByTestId('badge')
+
+        console.log(badgeElement.outerHTML)
+
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-rose-500')
+        
+    })
+    
+    it('should display the right text when order status is delivering', () => {
+        const wrapper = render(<OrderStatus status="delivering"/>)
+
+        wrapper.debug()
+
+        const statusText = wrapper.getByText('Em entrega')
+        const badgeElement = wrapper.getByTestId('badge')
+
+        console.log(badgeElement.outerHTML)
+
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-amber-500')
+        
+    })
+    
+    it('should display the right text when order status is processing', () => {
+        const wrapper = render(<OrderStatus status="processing"/>)
+
+        wrapper.debug()
+
+        const statusText = wrapper.getByText('Em preparo')
+        const badgeElement = wrapper.getByTestId('badge')
+
+        console.log(badgeElement.outerHTML)
+
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-amber-500')
+        
+    })
+    
+    it('should display the right text when order status is delivered', () => {
+        const wrapper = render(<OrderStatus status="delivered"/>)
+
+        wrapper.debug()
+
+        const statusText = wrapper.getByText('Entregue')
+        const badgeElement = wrapper.getByTestId('badge')
+
+        console.log(badgeElement.outerHTML)
+
+        expect(statusText).toBeInTheDocument()
+        expect(badgeElement).toHaveClass('bg-emerald-500')
+        
+    })
+})
--- aulas/04-pizzashop-web/vite.config.ts
+++ aulas/04-pizzashop-web/vite.config.ts
@@ -1,13 +1,21 @@
-import path from "node:path"
-import { defineConfig } from 'vite'
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="vitest" />
+import path from 'node:path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
   },
 })
