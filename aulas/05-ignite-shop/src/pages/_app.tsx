665bd9fbb34575a49950b3389e1652c7d7af1a04
Thiago Luiz <tl7thiago@gmail.com>
2024-08-15 18:51:19 -0300

    .

--- aulas/05-ignite-shop/src/pages/_app.tsx
+++ aulas/05-ignite-shop/src/pages/_app.tsx
@@ -1,21 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
-import { globalStyles } from "@/styles/global"
 import type { AppProps } from "next/app";
-
-import logoImg from '../assets/logo.svg'
-import { Container, Header } from "@/styles/pages/app";
-
-import Image from "next/image";
+import { CartProvider } from 'use-shopping-cart'
+import { Header } from '../components/Header'
+import { globalStyles } from "@/styles/global"
+import { Container } from "@/styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Container>
-            <Header>
-                <Image src={logoImg} alt="" />
-            </Header>
-            <Component {...pageProps} />
-        </Container>
+        <CartProvider
+        stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string}
+        cartMode="checkout-session"
+        currency="BRL"
+        shouldPersist
+        >
+            <Container>
+                <Header />
+                <Component {...pageProps} />
+            </Container>
+        </CartProvider>
     )
 }
---
+++ aulas/05-ignite-shop/src/pages/api/checkout.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
+
+interface FormattedData {
+  id: string
+  name: string
+  image: string
+  formattedPrice: string
+  quantity: number
+  priceId: string
+}
+
+export default async function checkout(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { formattedData } = req.body
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed.' })
+  }
+
+  if (!formattedData) {
+    return res.status(400).json({ error: 'CarDetails not found.' })
+  }
+
+  const lineItems = formattedData.map((item: FormattedData) => {
+    return {
+      price: item.priceId,
+      quantity: item.quantity,
+    }
+  })
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    line_items: lineItems,
+  })
+
+  console.log(checkoutSession)
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
+}
