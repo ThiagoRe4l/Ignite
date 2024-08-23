3a9a5bd5fffd654f5d73cee86d7fb47a24371f17
Thiago Luiz <tl7thiago@gmail.com>
2024-08-23 18:03:57 -0300

    .

---
+++ aulas/07-ignite-call/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  imagens: {
+    domains: ['https://lh3.googleusercontent.com/'],
+  },
+}
+
+export default nextConfig
---
+++ aulas/07-ignite-call/src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,73 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { ArrowRight, Check } from 'phosphor-react'
+import { Container, Header } from '../styles'
+import { AuthError, ConnectBox, Connectitem } from './styles'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+export default function ConnectCalendar() {
+  const session = useSession()
+  const router = useRouter()
+
+  const hasAuthError = !!router.query.error
+  const isSignedId = session.status === 'authenticated'
+
+  async function handleConnectCalendar() {
+    await signIn('google', { callbackUrl: '/register/connect-calendar' })
+  }
+
+  async function handleNavigateToNextStep() {
+    await router.push('/register/time-intervals')
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <Connectitem>
+          <Text>Google Calendar</Text>
+          {isSignedId ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
+        </Connectitem>
+
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar.
+          </AuthError>
+        )}
+
+        <Button
+          onClick={handleNavigateToNextStep}
+          type="submit"
+          disabled={!isSignedId}
+        >
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
